ff9b98844eaa56ac18346baa193f20f2a822f85f
LittlePuggers <anaim.ecologia@gmail.com>
2022-11-29 12:09:50 -0700

    Implementar botones funcionales para encode y decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,52 @@
 const cipher = {
-  // ...
-};
+  // Obtener el mensaje y offset(n)
+  // Implementar formula (x - 65 + n) % 26 + 65
+  // Hacer el return
 
+  //~.charCodeAt() te da el número del código ASCII
+  //String.fromCharCode() te da la letra correspondiente del ASCII
+
+  encode: (message, offset) => {
+    if (!message) {
+      return "";
+    } else {
+      const string = message.toUpperCase();
+      let newString = "";
+
+      for (let i = 0; i < string.length; i++) {
+        const char = string[i];
+        if (char.charCodeAt(0) <= 90 && char.charCodeAt(0) >= 65) {
+          const newChar = String.fromCharCode(
+            char.charCodeAt(0) - 65 + (parseInt(offset) % 26) + 65
+          );
+          newString += newChar;
+        } else {
+          newString += char;
+        }
+      }
+      return newString;
+    }
+  },
+  decode: (message, offset) => {
+    if (!message) {
+      return "";
+    }
+    const string = message.toUpperCase();
+    let newString = "";
+
+    for (let i = 0; i < string.length; i++) {
+      const char = string[i];
+      if (char.charCodeAt(0) <= 90 && char.charCodeAt(0) >= 65) {
+        const newChar = String.fromCharCode(
+          char.charCodeAt(0) - 65 - (parseInt(offset) % 26) + 65
+        );
+        newString += newChar;
+      } else {
+        newString += char;
+      }
+    }
+
+    return newString;
+  },
+};
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
-console.log(cipher);
+//console.log(cipher.encode("AB C,"));
+//console.log(cipher.decode("B C!D"));
+
+const message = document.getElementById("mensaje");
+const offset = document.getElementById("offset");
+const butencode = document.getElementById("bot-enc");
+const butdecode = document.getElementById("bot-dec");
+const result = document.getElementById("resultado");
+
+butencode.addEventListener("click", () => {
+  result.innerText = cipher.encode(message.value, offset.value);
+});
+butdecode.addEventListener("click", () => {
+  result.innerText = cipher.decode(message.value, offset.value);
+});
